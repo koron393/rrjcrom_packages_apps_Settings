LineageOS/android_packages_apps_Settings

Support multiple SAR values, separate args list with ','
Issue-Id: YAM-68

Change-Id: I21fcb3128a115388ed25d61daae88f4f3356929a

src/com/android/settings/deviceinfo/Status.java
@@ -731,9 +731,9 @@ public class Status extends PreferenceActivity {
 
     public static String getSarValues(Resources res) {
         String headLevel = String.format(res.getString(R.string.maximum_head_level,
-                res.getString(R.string.sar_head_level)));
+                TextUtils.split(res.getString(R.string.sar_head_level),",")));
         String bodyLevel = String.format(res.getString(R.string.maximum_body_level,
-                res.getString(R.string.sar_body_level)));
+                TextUtils.split(res.getString(R.string.sar_body_level),",")));
         return headLevel + "\n" + bodyLevel;
     }
 

koron393/rrjcrom_packages_apps_Settings

src/com/android/settings/deviceinfo/Status.java
@@ -731,9 +731,9 @@ public class Status extends PreferenceActivity {
 
     public static String getSarValues(Resources res) {
         String headLevel = String.format(res.getString(R.string.maximum_head_level,
-                res.getString(R.string.sar_head_level)));
+                TextUtils.split(res.getString(R.string.sar_head_level), ",")));
         String bodyLevel = String.format(res.getString(R.string.maximum_body_level,
-                res.getString(R.string.sar_body_level)));
+                TextUtils.split(res.getString(R.string.sar_body_level), ",")));
         return headLevel + "\n" + bodyLevel;
     }
 