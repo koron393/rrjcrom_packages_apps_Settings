LineageOS/android_packages_apps_Settings

Adding null checks to prevent OnPrepareRemoval() on null object
Adding NULL checks to parameters being sent with removePreference.
Issue-Id: YAM-107

Change-Id: I1ade95b0eb6b9a7e01df84ebba974a4d5224cfd8

src/com/android/settings/WirelessSettings.java
@@ -286,7 +286,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
         }
 
         // Remove NSD checkbox by default
-        getPreferenceScreen().removePreference(nsd);
+        if (nsd != null) getPreferenceScreen().removePreference(nsd);
         //mNsdEnabler = new NsdEnabler(activity, nsd);
 
         String toggleable = Settings.Global.getString(activity.getContentResolver(),
@@ -332,13 +332,13 @@ public class WirelessSettings extends SettingsPreferenceFragment
         // Remove NFC if not available
         mNfcAdapter = NfcAdapter.getDefaultAdapter(activity);
         if (mNfcAdapter == null) {
-            getPreferenceScreen().removePreference(nfc);
-            getPreferenceScreen().removePreference(androidBeam);
-            getPreferenceScreen().removePreference(nfcPayment);
+            if (nfc != null) getPreferenceScreen().removePreference(nfc);
+            if (androidBeam != null) getPreferenceScreen().removePreference(androidBeam);
+            if (nfcPayment != null) getPreferenceScreen().removePreference(nfcPayment);
             mNfcEnabler = null;
         } else if (!mPm.hasSystemFeature(PackageManager.FEATURE_NFC_HOST_CARD_EMULATION)) {
             // Only show if we have the HCE feature
-            getPreferenceScreen().removePreference(nfcPayment);
+            if (nfcPayment != null) getPreferenceScreen().removePreference(nfcPayment);
         }
 
         // Remove Mobile Network Settings and Manage Mobile Plan for secondary users,
@@ -374,7 +374,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
         final DevicePolicyManager mDPM = (DevicePolicyManager)
                 activity.getSystemService(Context.DEVICE_POLICY_SERVICE);
         // proxy UI disabled until we have better app support
-        getPreferenceScreen().removePreference(mGlobalProxy);
+        if (mGlobalProxy != null) getPreferenceScreen().removePreference(mGlobalProxy);
         mGlobalProxy.setEnabled(mDPM.getGlobalProxyAdmin() == null);
 
         // Disable Tethering if it's not allowed or if it's a wifi-only device

koron393/rrjcrom_packages_apps_Settings

src/com/android/settings/WirelessSettings.java
@@ -289,7 +289,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
         }
 
         // Remove NSD checkbox by default
-        getPreferenceScreen().removePreference(nsd);
+        if (nsd != null) getPreferenceScreen().removePreference(nsd);
         //mNsdEnabler = new NsdEnabler(activity, nsd);
 
         String toggleable = Settings.Global.getString(activity.getContentResolver(),
@@ -334,7 +334,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
         // Remove NFC if not available
         mNfcAdapter = NfcAdapter.getDefaultAdapter(activity);
         if (mNfcAdapter == null && nfcCategory != null) {
-            getPreferenceScreen().removePreference(nfcCategory);
+            if (nfcCategory != null) getPreferenceScreen().removePreference(nfcCategory);
             mNfcEnabler = null;
         }
 
@@ -371,7 +371,7 @@ public class WirelessSettings extends SettingsPreferenceFragment
         final DevicePolicyManager mDPM = (DevicePolicyManager)
                 activity.getSystemService(Context.DEVICE_POLICY_SERVICE);
         // proxy UI disabled until we have better app support
-        getPreferenceScreen().removePreference(mGlobalProxy);
+        if (mGlobalProxy != null) getPreferenceScreen().removePreference(mGlobalProxy);
         mGlobalProxy.setEnabled(mDPM.getGlobalProxyAdmin() == null);
 
         // Disable Tethering if it's not allowed or if it's a wifi-only device