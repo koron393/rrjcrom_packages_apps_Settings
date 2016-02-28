LineageOS/android_packages_apps_Settings

Lockscreen: Show redaction interstitial when swipe selected
If the user is switching from a secure lock screen to an insecure
one and they have chosen to redact sensitive/all notififcations,
show the RedactionInterstitial so the user has the option to change
this setting.

Change-Id: I2bcfd01fb322eb66a10a0f29feb9da696c524cdf
TICKET: YAM-103

src/com/android/settings/ChooseLockGeneric.java
@@ -32,6 +32,7 @@ import android.os.Process;
 import android.os.UserHandle;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 import android.security.KeyStore;
 import android.hardware.fingerprint.Fingerprint;
 import android.hardware.fingerprint.FingerprintManager;
@@ -46,6 +47,7 @@ import android.widget.Toast;
 
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.widget.LockPatternUtils;
+import com.android.settings.notification.RedactionInterstitial;
 
 public class ChooseLockGeneric extends SettingsActivity {
     public static final String CONFIRM_CREDENTIALS = "confirm_credentials";
@@ -470,7 +472,7 @@ public class ChooseLockGeneric extends SettingsActivity {
             if (!mPasswordConfirmed) {
                 throw new IllegalStateException("Tried to update password without confirming it");
             }
-
+            boolean wasSecure = mLockPatternUtils.isSecure(UserHandle.myUserId());
             quality = upgradeQuality(quality);
 
             final Context context = getActivity();
@@ -503,6 +505,9 @@ public class ChooseLockGeneric extends SettingsActivity {
                 mChooseLockSettingsHelper.utils().clearLock(UserHandle.myUserId());
                 mChooseLockSettingsHelper.utils().setLockScreenDisabled(disabled,
                         UserHandle.myUserId());
+                if (wasSecure) {
+                    maybeShowRedactionInterstitial();
+                }
                 removeAllFingerprintTemplatesAndFinish();
                 getActivity().setResult(Activity.RESULT_OK);
             } else {
@@ -516,6 +521,19 @@ public class ChooseLockGeneric extends SettingsActivity {
                 mFingerprintManager.remove(new Fingerprint(null, 0, 0, 0), mRemovalCallback);
             } else {
                 finish();
+            }
+        }
+
+        private void maybeShowRedactionInterstitial() {
+            final boolean enabled = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS, 0) != 0;
+            final boolean show = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS, 1) != 0;
+            if (!(enabled && show)) {
+                Intent intent = RedactionInterstitial.createStartIntent(getContext());
+                if (intent != null) {
+                    startActivity(intent);
+                }
             }
         }
 

koron393/rrjcrom_packages_apps_Settings

src/com/android/settings/ChooseLockGeneric.java
@@ -34,6 +34,7 @@ import android.os.Process;
 import android.os.UserManager;
 import android.preference.Preference;
 import android.preference.PreferenceScreen;
+import android.provider.Settings;
 import android.security.KeyStore;
 import android.util.EventLog;
 import android.util.Log;
@@ -46,6 +47,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.android.internal.widget.LockPatternUtils;
+import com.android.settings.notification.RedactionInterstitial;
 
 import java.util.List;
 
@@ -569,10 +571,27 @@ public class ChooseLockGeneric extends SettingsActivity {
             } else if (quality == DevicePolicyManager.PASSWORD_QUALITY_UNSPECIFIED) {
                 mChooseLockSettingsHelper.utils().clearLock(false);
                 mChooseLockSettingsHelper.utils().setLockScreenDisabled(disabled);
+                maybeShowRedactionInterstitial();
                 getActivity().setResult(Activity.RESULT_OK);
                 finish();
             } else {
                 finish();
+            }
+        }
+
+        private void maybeShowRedactionInterstitial() {
+            // do nothing if lock screen disabled
+            if (mLockPatternUtils.isLockScreenDisabled()) return;
+
+            final boolean enabled = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_SCREEN_SHOW_NOTIFICATIONS, 0) != 0;
+            final boolean show = Settings.Secure.getInt(getContentResolver(),
+                    Settings.Secure.LOCK_SCREEN_ALLOW_PRIVATE_NOTIFICATIONS, 1) != 0;
+            if (!(enabled && show)) {
+                Intent intent = RedactionInterstitial.createStartIntent(getActivity());
+                if (intent != null) {
+                    startActivity(intent);
+                }
             }
         }
 